549d07b88c8d4e92435c7d3992532049a4c1900f
Raphaël Aussenac <raphael.aussenac@gmail.com>
2020-04-27 18:10:10 +0200

    evaluation at sp level

--- 1_indexCalc.R
+++ 1_indexCalc.R
@@ -5,6 +5,10 @@
 # clean up environment
 rm(list = ls())
 
+# library
+library(plyr)
+library(data.table)
+
 # Choose the work directory = folder
 if (Sys.info()["sysname"] == "Darwin"){
   setwd("/Users/raphaelaussenac/Documents/GitHub/modEval/modelsOutput")
@@ -15,48 +19,136 @@ if (Sys.info()["sysname"] == "Darwin"){
 # retrieve list of file (simulations and observations)
 fileNames <- Sys.glob('*.csv')
 
+# load data and assign model and site names to it
+listsrc <- c('profound', 'landclim', '4c')
+listsite <- c('kroof', 'solling_beech', 'solling_spruce')
+alldf <- data.frame()
+for (src in listsrc){
+  for (i in grep(pattern = src, x = fileNames)){
+    temp <- read.csv(fileNames[i])
+    # detect site
+    for (s in listsite){
+      if(grepl(pattern = s, x = fileNames[i])){site <- s}
+    }
+    temp$src <- src
+    temp$site <- site
+    alldf <- rbind(alldf, temp)
+  }
+}
+
+
 ################################################################################
 # calculate yearly aggregated index (N, Dg, etc) for observed and predicted data
+# at stand level
 ################################################################################
 
-for (i in fileNames){
+# stand N, Dg, H, BA
+stand <- ddply(alldf, .(year, src, site), summarise, N = length(D),
+                                                 Dg = sqrt(sum(D^2)/length(D)),
+                                                 H = sum(H) / length(H),
+                                                 BA = sum(pi * D^2 / 4) / 10000)
+# stand BAI
+# first define lag function
+lg <- function(x)c(NA, x[1:(length(x)-1)])
+# convert stand into data.table format
+stand <- data.table(stand)
+# create lagged variable
+stand[,BAI := lg(BA), by = c('src', 'site')]
+stand$BAI <- stand$BA - stand$BAI
+stand$species <- 'allsp'
+stand <- stand[, c('year', 'src', 'site', 'species', 'N', 'Dg', 'H', 'BA', 'BAI')]
 
-  # load file
-  site <- read.csv(i)
+# write site index
+# if (Sys.info()["sysname"] == "Darwin"){
+#   write.csv(stand, file = paste('/Users/raphaelaussenac/Documents/GitHub/modEval/indexForEval/evalVar.csv', sep = ''), row.names = FALSE)
+# } else if (Sys.info()["sysname"] == "Windows"){
+#   write.csv(stand, file = paste('C:/Users/raphael.aussenac/Documents/GitHub/modEval/indexForEval/evalVar.csv', sep = ''), row.names = FALSE)
+# }
 
-  # down to the year level
-  df <- data.frame()
-  for (yr in unique(site$year)){
-    yrdf <- site[site$year == yr, ]
+################################################################################
+# calculate yearly aggregated index (N, Dg, etc) for observed and predicted data
+# at sp level
+################################################################################
 
-    # Number of trees ----------------------------------------------------------
-    yrdf$N <- nrow(yrdf)
+# stand N, Dg, H, BA
+sp <- ddply(alldf, .(year, src, site, species), summarise, N = length(D),
+                                                 Dg = sqrt(sum(D^2)/length(D)),
+                                                 H = sum(H) / length(H),
+                                                 BA = sum(pi * D^2 / 4) / 10000)
+# stand BAI
+# convert stand into data.table format
+sp <- data.table(sp)
+# create lagged variable
+sp[,BAI := lg(BA), by = c('src', 'site', 'species')]
+sp$BAI <- sp$BA - sp$BAI
+sp <- rbind(stand, sp)
 
-    # mean quadratic diameter --------------------------------------------------
-    yrdf$Dg <- sqrt(sum(yrdf$D^2)/nrow(yrdf))
-
-    # arithmetic mean height ---------------------------------------------------
-    yrdf$H <- sum(yrdf$H) / nrow(yrdf)
-
-    # basal area ---------------------------------------------------------------
-    yrdf$BA <- sum(pi * yrdf$D^2 / 4) / 10000 # convert in m2
-
-    # save
-    yrdf <- yrdf[1, c('year', 'N', 'Dg', 'H', 'BA')]
-    df <- rbind(df, yrdf)
-  }
-
-  # basal area increment -------------------------------------------------------
-  df$BAI <- c(NA, df$BA[-length(df$BA)])
-  df$BAI <- df$BA - df$BAI
-
-  # write site index
-  if (Sys.info()["sysname"] == "Darwin"){
-    write.csv(df, file = paste('/Users/raphaelaussenac/Documents/GitHub/modEval/indexForEval/',
-                                    i, sep = ''), row.names = FALSE)
-  } else if (Sys.info()["sysname"] == "Windows"){
-    write.csv(df, file = paste('C:/Users/raphael.aussenac/Documents/GitHub/modEval/indexForEval/',
-                                    i, sep = ''), row.names = FALSE)
-  }
-
+# write site index
+if (Sys.info()["sysname"] == "Darwin"){
+  write.csv(sp, file = paste('/Users/raphaelaussenac/Documents/GitHub/modEval/indexForEval/evalVarSp.csv', sep = ''), row.names = FALSE)
+} else if (Sys.info()["sysname"] == "Windows"){
+  write.csv(sp, file = paste('C:/Users/raphael.aussenac/Documents/GitHub/modEval/indexForEval/evalVarSp.csv', sep = ''), row.names = FALSE)
 }
+
+
+
+
+
+
+#
+# stand[stand$src == 'profound' & stand$site == "kroof",]
+#
+#
+#
+# # list of species
+# specieslist <- unique(alldf$species)
+#
+# #
+# for (src in listsrc){
+#   # at each site
+#   for (s in listsite){
+#     site <- alldf[alldf$src == src & alldf$site == s, ]
+#     # down to the year level
+#     df <- data.frame()
+#     for (yr in unique(site$year)){
+#       yrdf <- site[site$year == yr, ]
+#
+#       # Number of trees --------------------------------------------------------
+#       yrdf$N <- nrow(yrdf)
+#       # per sepcies at kroof site
+#       # if (s == 'kroof'){
+#       #   sp <- ddply(yrdf, .(species), summarise, Nsp = length(D))
+#       # }
+#
+#       # mean quadratic diameter ------------------------------------------------
+#       yrdf$Dg <- sqrt(sum(yrdf$D^2)/nrow(yrdf))
+#
+#       # arithmetic mean height -------------------------------------------------
+#       yrdf$H <- sum(yrdf$H) / nrow(yrdf)
+#
+#       # basal area -------------------------------------------------------------
+#       yrdf$BA <- sum(pi * yrdf$D^2 / 4) / 10000 # convert in m2
+#
+#       # save
+#       yrdf <- yrdf[1,]
+#       yrdf[, c('species', 'D')] <- NULL
+#
+#       # yrdf <- yrdf[1, c('year', 'N', 'Dg', 'H', 'BA')]
+#       df <- rbind(df, yrdf)
+#     }
+#
+#     # basal area increment -------------------------------------------------------
+#     df$BAI <- c(NA, df$BA[-length(df$BA)])
+#     df$BAI <- df$BA - df$BAI
+#
+#     # write site index
+#     if (Sys.info()["sysname"] == "Darwin"){
+#       write.csv(df, file = paste('/Users/raphaelaussenac/Documents/GitHub/modEval/indexForEval/',
+#                                       src, '_', s, '.csv', sep = ''), row.names = FALSE)
+#     } else if (Sys.info()["sysname"] == "Windows"){
+#       write.csv(df, file = paste('C:/Users/raphael.aussenac/Documents/GitHub/modEval/indexForEval/',
+#                                       src, '_', s, '.csv', sep = ''), row.names = FALSE)
+#     }
+#
+#   }
+# }
--- 2_eval.R
+++ 2_eval.R
@@ -17,25 +17,8 @@ if (Sys.info()["sysname"] == "Darwin"){
   setwd("C:/Users/raphael.aussenac/Documents/GitHub/modEval/indexForEval")
 }
 
-# load files
-fileNames <- Sys.glob('*.csv')
-
-# load data and assign model and site names to it
-listsrc <- c('profound', 'landclim', '4c')
-listsite <- c('kroof', 'solling_beech', 'solling_spruce')
-alldf <- data.frame()
-for (src in listsrc){
-  for (i in grep(pattern = src, x = fileNames)){
-    temp <- read.csv(fileNames[i])
-    # detect site
-    for (s in listsite){
-      if(grepl(pattern = s, x = fileNames[i])){site <- s}
-    }
-    temp$src <- src
-    temp$site <- site
-    alldf <- rbind(alldf, temp)
-  }
-}
+alldf <- read.csv('evalVarSp.csv')
+alldf <- alldf[order(alldf$src, alldf$site, alldf$species, alldf$year),]
 
 # manage different plot scales
 # kroof is 0.5ha while other sites are 1ha
@@ -55,72 +38,78 @@ evalVar <- c('N', 'Dg', 'H', 'BA', 'BAI')
 
 evaluation <- data.frame()
 # for each model
-for (mod in listsrc[listsrc != 'profound']){
+for (mod in unique(alldf$src)[unique(alldf$src) != 'profound']){
 
   # at each site
-  for (site in listsite){
-    # create df with index values
-    obs <- alldf[alldf$src == 'profound' & alldf$site == site, ]
-    colnames(obs)[!(colnames(obs) %in% c('year', 'src', 'site'))] <- paste('obs', colnames(obs)[!(colnames(obs) %in% c('year', 'src', 'site'))], sep = '')
-    obs[, c('src', 'site')] <- NULL
-    pred <- alldf[alldf$src == mod & alldf$site == site, ]
-    colnames(pred)[!(colnames(pred) %in% c('year', 'src', 'site'))] <- paste('pred', colnames(pred)[!(colnames(pred) %in% c('year', 'src', 'site'))], sep = '')
-    df <- merge(obs, pred, by = 'year')
+  for (site in unique(alldf$site)){
+    for (sp in unique(alldf$species)){
+      # create df with index values
+      obs <- alldf[alldf$src == 'profound' & alldf$site == site & alldf$species == sp, ]
+      colnames(obs)[!(colnames(obs) %in% c('year', 'src', 'site', 'species'))] <- paste('obs', colnames(obs)[!(colnames(obs) %in% c('year', 'src', 'site', 'species'))], sep = '')
+      obs[, c('src', 'site', 'species')] <- NULL
+      pred <- alldf[alldf$src == mod & alldf$site == site & alldf$species == sp, ]
+      colnames(pred)[!(colnames(pred) %in% c('year', 'src', 'site', 'species'))] <- paste('pred', colnames(pred)[!(colnames(pred) %in% c('year', 'src', 'site', 'species'))], sep = '')
+      df <- merge(obs, pred, by = 'year')
 
-    # for each index to compare
-    deviancedf <- data.frame('variable'= NA, 'devMeasure'= NA, 'value' = NA)
-    for (i in evalVar){
-      # no evaluation of H at kroof site
-      if (i != 'H' | site != 'kroof'){
-        # create evaluation df
-        evaldf <- df[, c('year', colnames(df)[colnames(df) == paste('obs', i, sep = '') | colnames(df) == paste('pred', i, sep = '')])]
-        colnames(evaldf)[substr(colnames(evaldf), 1, 3) == 'obs'] <- 'Y'
-        colnames(evaldf)[substr(colnames(evaldf), 1, 4) == 'pred'] <- 'X'
-        # remove NA values (for BAI and H)
-        if (i == 'BAI'){
-          evaldf <- evaldf[!is.na(evaldf$X), ]
-          evaldf <- evaldf[!is.na(evaldf$Y), ]
+      # if species present
+      if (nrow(df) > 0){
+        # for each index to compare
+        deviancedf <- data.frame('variable'= NA, 'devMeasure'= NA, 'value' = NA)
+        for (i in evalVar){
+          # no evaluation of H at kroof site
+          if (i != 'H' | site != 'kroof'){
+            # create evaluation df
+            evaldf <- df[, c('year', colnames(df)[colnames(df) == paste('obs', i, sep = '') | colnames(df) == paste('pred', i, sep = '')])]
+            colnames(evaldf)[substr(colnames(evaldf), 1, 3) == 'obs'] <- 'Y'
+            colnames(evaldf)[substr(colnames(evaldf), 1, 4) == 'pred'] <- 'X'
+            # remove NA values (for BAI and H)
+            if (i == 'BAI'){
+              evaldf <- evaldf[!is.na(evaldf$X), ]
+              evaldf <- evaldf[!is.na(evaldf$Y), ]
+            }
+            # calculate x, y and x*y, b and r²
+            evaldf$x <- evaldf$X - mean(evaldf$X)
+            evaldf$y <- evaldf$Y - mean(evaldf$Y)
+            evaldf$xy <-evaldf$x * evaldf$y
+            model <- lm(evaldf$Y ~ evaldf$X)
+            b <- as.numeric(coef(model)[2]) # = sum(evaldf$xy) / sum(evaldf$x^2)
+            r2 <- summary(model)$r.squared # = ( sum(evaldf$xy)^2 ) / ( sum(evaldf$x^2)*sum(evaldf$y^2) )
+            # calculate MSD and its 3 components
+            MSD <- sum( (evaldf$X - evaldf$Y) ^2) / nrow(evaldf)
+            SB <- (mean(evaldf$X) - mean(evaldf$Y))^2
+            NU <- ((1 - b)^2) * ( sum(evaldf$x^2) / nrow(evaldf) )
+            LC <- (1 - r2) * ( sum(evaldf$y^2) / nrow(evaldf) )
+            # save
+            deviancedf <- rbind(deviancedf, c(i, 'SB', SB))
+            deviancedf <- rbind(deviancedf, c(i, 'NU', NU))
+            deviancedf <- rbind(deviancedf, c(i, 'LC', LC))
+          }
         }
-        # calculate x, y and x*y, b and r²
-        evaldf$x <- evaldf$X - mean(evaldf$X)
-        evaldf$y <- evaldf$Y - mean(evaldf$Y)
-        evaldf$xy <-evaldf$x * evaldf$y
-        model <- lm(evaldf$Y ~ evaldf$X)
-        b <- as.numeric(coef(model)[2]) # = sum(evaldf$xy) / sum(evaldf$x^2)
-        r2 <- summary(model)$r.squared # = ( sum(evaldf$xy)^2 ) / ( sum(evaldf$x^2)*sum(evaldf$y^2) )
-        # calculate MSD and its 3 components
-        MSD <- sum( (evaldf$X - evaldf$Y) ^2) / nrow(evaldf)
-        SB <- (mean(evaldf$X) - mean(evaldf$Y))^2
-        NU <- ((1 - b)^2) * ( sum(evaldf$x^2) / nrow(evaldf) )
-        LC <- (1 - r2) * ( sum(evaldf$y^2) / nrow(evaldf) )
+        deviancedf$value <- as.numeric(deviancedf$value)
+        deviancedf$mod <- mod
+        deviancedf$site <- site
+        deviancedf$species <- sp
+        deviancedf <- deviancedf[-1,]
         # save
-        deviancedf <- rbind(deviancedf, c(i, 'SB', SB))
-        deviancedf <- rbind(deviancedf, c(i, 'NU', NU))
-        deviancedf <- rbind(deviancedf, c(i, 'LC', LC))
+        evaluation <- rbind(evaluation, deviancedf)
       }
     }
-    deviancedf$value <- as.numeric(deviancedf$value)
-    deviancedf$mod <- mod
-    deviancedf$site <- site
-    deviancedf <- deviancedf[-1,]
-    # save
-    evaluation <- rbind(evaluation, deviancedf)
   }
 }
 
 
 ################################################################################
-# evaluation and time series
+# evaluation and time series at stand level
 ################################################################################
 
 # evaluation
-evaluation$site = factor(evaluation$site, levels = listsite)
-evaluation$mod = factor(evaluation$mod, levels = listsrc)
+evaluation$site = factor(evaluation$site, levels = unique(alldf$site))
+evaluation$mod = factor(evaluation$mod, levels = unique(alldf$src))
 evaluation$variable = factor(evaluation$variable, levels = evalVar)
 
-ggplot(data = evaluation, aes(x = mod, y = value, fill = devMeasure)) +
+ggplot(data = evaluation[evaluation$species == 'allsp',], aes(x = mod, y = value, fill = devMeasure)) +
 geom_bar(stat = "identity") +
-facet_grid(variable ~ site, scale = "free", drop = FALSE) +
+facet_grid(variable ~ site, scale = "free") +
 theme_light() +
 theme(panel.grid.minor = element_blank(),
       # panel.grid.major = element_blank(),
@@ -133,13 +122,15 @@ ggsave(paste('eval.jpg', sep = '_'), width = 8, height = 8)
 
 
 # time series
-ts <- melt(alldf, id.vars = c("year", "src", "site"))
-ts$site = factor(ts$site, levels = listsite)
-ts$src = factor(ts$src, levels = listsrc)
+stand <- alldf[alldf$species == 'allsp',]
+stand$species <- NULL
+ts <- melt(stand, id.vars = c("year", "src", "site"))
+ts$site = factor(ts$site, levels = unique(alldf$site))
+ts$src = factor(ts$src, levels = unique(alldf$src))
 ts$variable = factor(ts$variable, levels = evalVar)
 
 ggplot() +
-geom_line(data = ts[ts$src == 'profound',], aes(x = year, y = value), col = 'black') +
+geom_line(data = ts[ts$src == 'profound',], aes(x = year, y = value, col = src)) +
 geom_line(data = ts[ts$src != 'profound',], aes(x = year, y = value, col = src), linetype = 'dashed') +
 facet_grid(variable ~ site, scale = "free") +
 theme_light() +
@@ -151,3 +142,45 @@ theme(panel.grid.minor = element_blank(),
       legend.title=element_blank(),
       panel.spacing = unit(20, 'pt'))
 ggsave(paste('ts.jpg', sep = '_'), width = 8, height = 8)
+
+
+################################################################################
+# evaluation and time series at sp level
+################################################################################
+
+# evaluation
+sp <- evaluation[evaluation$site == 'kroof' & evaluation$sp != "allsp",]
+ggplot(data = sp, aes(x = mod, y = value, fill = devMeasure)) +
+geom_bar(stat = "identity") +
+facet_grid(variable ~ species, scale = "free") +
+theme_light() +
+theme(panel.grid.minor = element_blank(),
+      # panel.grid.major = element_blank(),
+      strip.background = element_blank(),
+      strip.text = element_text(colour = 'black'),
+      legend.position = "bottom",
+      legend.title=element_blank(),
+      panel.spacing = unit(20, 'pt'))
+ggsave(paste('evalSpKroof.jpg', sep = '_'), width = 8, height = 8)
+
+
+# time series
+sp <- alldf[alldf$site == 'kroof' & alldf$species != 'allsp',]
+ts <- melt(sp, id.vars = c("year", "src", 'site', "species"))
+ts$site = factor(ts$site, levels = unique(alldf$site))
+ts$src = factor(ts$src, levels = unique(alldf$src))
+ts$variable = factor(ts$variable, levels = evalVar)
+
+ggplot() +
+geom_line(data = ts[ts$src == 'profound',], aes(x = year, y = value, col = src)) +
+geom_line(data = ts[ts$src != 'profound',], aes(x = year, y = value, col = src), linetype = 'dashed') +
+facet_grid(variable ~ species, scale = "free") +
+theme_light() +
+theme(panel.grid.minor = element_blank(),
+      # panel.grid.major = element_blank(),
+      strip.background = element_blank(),
+      strip.text = element_text(colour = 'black'),
+      legend.position = "bottom",
+      legend.title=element_blank(),
+      panel.spacing = unit(20, 'pt'))
+ggsave(paste('tsSpKroof.jpg', sep = '_'), width = 8, height = 8)
