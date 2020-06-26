6d09cc796edd213e790b8aa4ea55961ffccf66b3
Arnaud Guyennon <arnaud.guyennon@irstea.fr>
2020-06-26 15:36:52 +0200

    Change setwd

--- plan.R
+++ plan.R
@@ -6,17 +6,16 @@ library(fmsb)
 library(plyr)
 library(viridis)
 
-source("R/assembleDataAndSim.R")
-source("R/indexCalc.R")
-source("R/evalPlot.R")
-source('R/HetIndex.R')
-
 if (Sys.info()["sysname"] == "Darwin"){
   setwd("/Users/raphaelaussenac/Documents/GitHub/modEval")
 } else if (Sys.info()["sysname"] == "Windows"){
   setwd("C:/Users/raphael.aussenac/Documents/GitHub/modEval")
 }
 
+source("R/assembleDataAndSim.R")
+source("R/indexCalc.R")
+source("R/evalPlot.R")
+source('R/HetIndex.R')
 plan <- drake_plan(
     IsDataFileProfound = WriteDataSim('profound'),
     IsDataIndexProfound = WriteIndex('profound'),
