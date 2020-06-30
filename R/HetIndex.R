80e1128b6c5e6a5bfde8191c0e2002c342652d6a
Arnaud Guyennon <arnaud.guyennon@irstea.fr>
2020-06-30 09:57:10 +0200

    Modified to be in line with assembleDataSim

--- R/HetIndex.R
+++ R/HetIndex.R
@@ -17,14 +17,33 @@ ChooseVar <- function(dataSet, Nvar = "D_cm", Inter = 10){
 CalcHill <- function(dataSet){
 # dataSet after ChooseVar, return GS : Gini-Simpson, Sh : Shannon, N : Nb for each year
     if (is.null(dataSet[["Var"]])){stop('Need to choose variable first')}
-    PClass <- group_by(dataSet, year) %>% mutate(N = n()) %>% ungroup() %>%
-	    group_by(year, Class) %>% summarise(p=n()/N[1]) %>% ungroup()
-    HillNB <- group_by(PClass, year) %>% summarise(Sh=-sum(p * log(p)),
+    PClass <- group_by(dataSet, year, site, src) %>% mutate(N = sum(weight)) %>% ungroup() %>%
+	    group_by(year, Class, site, src) %>% summarise(p=(sum(weight)/N[1])) %>% ungroup()
+    HillNB <- group_by(PClass, year, site, src) %>% summarise(Sh=-sum(p * log(p)),
 	    N=n(), GS=1-sum(p^2), Simp=sum(p^2)) %>% ungroup()
     return(HillNB)
 }
 
-ReturnHill <- function(Nvar='D_cm', model='4c', site='kroof', Inter=10, path='.'){
+ReturnHill <- function(evalSite, Nvar='D_cm', Inter=10, path='.'){
+    if (!file.exists(paste0('data/all_',evalSite,'.csv'))){stop('Need to build dataset first')}
+    dataTemp <- read.csv(file=paste0('data/all_',evalSite,'.csv'))
+    dataTemp <- ChooseVar(dataTemp, Nvar=Nvar, Inter=Inter)
+    Hill <- CalcHill(dataTemp)
+    return(Hill)
+}
+
+Example <- function(){
+	require(ggplot2)
+    Nvar <- "D_cm"
+    Inter <- 10
+    model <- c('profound', '4c', 'landclim', 'Salem')
+    site <- c('kroof','solling-spruce', 'solling-beech')
+    OUT <- ReturnHill(Nvar, model, site, Inter)
+    ggplot(OUT, aes(x=year, y=Sh, col=model)) + geom_line() +
+        facet_wrap(~site) + ggtitle(paste(Nvar,"Shannon"))
+}
+
+ReturnHillOld <- function(Nvar='D_cm', model='4c', site='kroof', Inter=10, path='.'){ # Former dataset  implementation, kept for security
 	# model and site can be list, Nvar cannot
     listfiles <- intersect(list.files(path=path, pattern=paste(model,collapse='|')),
         list.files(path=path, pattern=paste(site,collapse='|')))
@@ -41,13 +60,3 @@ ReturnHill <- function(Nvar='D_cm', model='4c', site='kroof', Inter=10, path='.'
     return(Hill)
 }
 
-Example <- function(){
-	require(ggplot2)
-    Nvar <- "D_cm"
-    Inter <- 10
-    model <- c('profound', '4c', 'landclim', 'Salem')
-    site <- c('kroof','solling-spruce', 'solling-beech')
-    OUT <- ReturnHill(Nvar, model, site, Inter)
-    ggplot(OUT, aes(x=year, y=Sh, col=model)) + geom_line() +
-        facet_wrap(~site) + ggtitle(paste(Nvar,"Shannon"))
-}
