d7ca91a6fe8652403525f1de6536b1e8b989b4ba
Arnaud Guyennon <arnaud.guyennon@irstea.fr>
2020-08-18 09:47:59 +0200

    add new standVarCalc function to test

--- R/standVarCalc.R
+++ R/standVarCalc.R
@@ -2,6 +2,38 @@
 library(plyr)
 library(data.table)
 
+standVarCalcNew <- function(evalSite){
+  alldf <- read.csv(file.path('DATA',paste0('all_', evalSite, '.csv')))
+  YearlySP <- group_by(alldf, year, src, site, species) %>%
+        dplyr::summarise(N = sum(weight),
+                  Dg = sqrt(sum(D_cm^2 * weight)/sum(weight)),
+                  H = sum(H_m * weight) /sum(weight),
+                  BA = sum((pi * (D_cm/200)^2) * weight),
+                  V = sum(V_m3 * weight)) %>% ungroup()
+  YearlySt <- group_by(alldf, year, src, site) %>%
+        dplyr::summarise(N = sum(weight),
+                  Dg = sqrt(sum(D_cm^2 * weight)/sum(weight)),
+                  H = sum(H_m * weight) /sum(weight),
+                  BA = sum((pi * (D_cm/200)^2) * weight),
+                  V = sum(V_m3 * weight)) %>% ungroup() %>% mutate(species='allsp')
+  Yearly <- rbind(YearlySP, YearlySt)
+  YearlyObs <- group_by(Yearly, site) %>% dplyr::filter(year %in% unique(year[src=='data'])) %>% ungroup()
+  YearlyNonObs <- group_by(Yearly, site) %>% dplyr::filter(!(year %in% unique(year[src=='data']))) %>% ungroup()
+  yb <- function(year){
+        if (length(year)>1){
+                return(c(NA,year[1:(length(year)-1)]))
+        }else{
+                return(NA)
+        }
+  }
+  DecYear <- 0
+  if (evalSite=='bauges'){DecYear <- 1}
+  YearlyObs <- group_by(YearlyObs, site, src, species) %>%
+        dplyr::mutate(yearbefore=yb(year), BAbefore=yb(BA), BAI_yr=(BA-BAbefore)/(year-yearbefore+DecYear)) %>%
+        ungroup() %>% dplyr::select(-yearbefore, -BAbefore)
+  return(rbind(YearlyObs, mutate(YearlyNonObs, BAI_yr=NA)))
+}
+
 standVarCalc <- function(evalSite){
 # load data
   alldf <- read.csv(paste0('./data/all_', evalSite, '.csv'))
